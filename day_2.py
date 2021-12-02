966960d821ec4d316f335ba0215c1aafef0f451b
Carlo Fazioli <cfazzy@gmail.com>
2021-12-02 00:16:59 -0500

    Day 2 solution

--- day_2.py
+++ day_2.py
@@ -6,34 +6,29 @@ from utilities import load_data, submit
 input_data = load_data()
 
 # Split input into a list of str:
-# input_data = input_data.splitlines()
+input_data = input_data.splitlines()
 
 # Convert to list of int:
 # input_data = list(map(int, input_data.splitlines()))
 
 
 def main():
-    ############################################
-    # Part 1
-    ############################################
+    horizontal = 0
+    depth = 0
+    aim = 0
+    for instruction in input_data:
+        a, x = instruction.split(' ')
+        x = int(x)
+        if a == 'forward':
+            horizontal += x
+            depth += aim*x
+        if a == 'down':
+            aim += x
+        if a == 'up':
+            aim -= x
 
-    part = 1
-
-    # Code here
-
-    answer = None
-    submit(answer, part)
-
-    ############################################
-    # Part 2
-    ############################################
-
-    part = 2
-
-    # Code here
-
-    answer = None
-    submit(answer, part)
+    answer = horizontal*depth
+    print(answer)
 
 
 if __name__ == '__main__':
--- utilities.py
+++ utilities.py
@@ -4,18 +4,21 @@ import requests
 
 YEAR = datetime.today().year
 DAY = datetime.today().day
+midnight = datetime(YEAR, 12, DAY, 0, 0, 0, 0)
 
 
 def load_data(year=YEAR, day=DAY):
     input_file = Path(f'./input_data/day_{day}.txt')
-    if not input_file.exists():
+    if input_file.exists():
+        print('Cached input file found.  Loading...')
+    else:
+        print('No local input file. Downloading...')
         with open('./.aoc_cookie', 'r') as f:
             aoc_cookie = f.read().strip()
         url = f'https://adventofcode.com/{year}/day/{day}/input'
         response = requests.get(url, cookies={'session': aoc_cookie})
         with open(input_file, 'w') as f:
             f.write(response.text)
-
     with open(input_file, 'r') as f:
         input_data = f.read().strip()
     return input_data
@@ -28,7 +31,9 @@ def submit(answer, part, year=YEAR, day=DAY):
         aoc_cookie = f.read().strip()
     response = requests.post(url, payload, cookies={'session': aoc_cookie})
     if 'That\'s the right answer!' in response.text:
+        dt = datetime.now() - midnight
         print(f'Fucken sick bro you got part {part}!')
+        print(f'Time taken: {dt}')
     elif 'You don\'t seem to be solving the right level.' in response.text:
         print(f'Ya already solved part {part} knucklehead.')
     else:
